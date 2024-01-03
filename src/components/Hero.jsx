5c46a2fc10d9a73f492986153225e61acc46cb48
Evan Honggo Widjojo <evanedreo@gmail.com>
2024-01-02 23:52:00 -0800

    updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         <span className="orange_gradient ">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Save your time reading an article, a free tool for summarizing articles, making long texts brief and straightforward. <i className="text-white">Please wait for around 10 seconds</i>
+        Save your time reading an article, a free tool for summarizing articles, making long texts brief and straightforward. <i className="text-purple-700">Please wait for around 10 seconds</i>
       </h2>
     </header>
   );
